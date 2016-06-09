d28744ce781569960710ccecbcafd2217e20b4ff
ChinatsuKawakami <ckawakam@my.centennialcollege.ca>
2016-06-09 11:25:16 -0400

    Added ShuffledDeck method in driver class

--- COMP123-S2016-CKawakami-300821245-Lab3/Program.cs
+++ COMP123-S2016-CKawakami-300821245-Lab3/Program.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
  * Student ID : 300821245
  * Date : 9th June 2016
  * Modified Date : 9th June 2016
- * version 0.0.3 - Added DisplayDeck method and CreateDeck method in driver class
+ * version 0.0.4 - Added ShuffledDeck method in driver class
  *
  */
 namespace COMP123_S2016_CKawakami_300821245_Lab3
@@ -28,8 +28,12 @@ namespace COMP123_S2016_CKawakami_300821245_Lab3
 
             //List that represents deck of cards
         List <Card> Deck = new List<Card>();
+
             CreateDeck(Deck);
-            DisplayDeck(Deck);
+            DisplayDeck(Deck);//display initial state of deck
+
+            ShuffleDeck(Deck);
+            DisplayDeck(Deck);//display the Shuffled state of deck
             
   
         }//close Main
@@ -75,6 +79,15 @@ namespace COMP123_S2016_CKawakami_300821245_Lab3
 
         }//close _create Method
 
+
+        /**
+         * <summary>
+         * This method displays a list of card objects to the console
+         * </summary>
+         * @method DisplayDeck
+         * @params {List<Card>} deck
+         * @returns {void}
+         */
         public static void DisplayDeck(List<Card> deck)
         {
             Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++++++++");
@@ -88,6 +101,37 @@ namespace COMP123_S2016_CKawakami_300821245_Lab3
             Console.WriteLine();
 
         }//close DisplayDeck method
+
+        /**
+         * <summary>
+         * This method randomly shuffles a list of Card object
+         *
+         * </summary>
+         * @method ShuffledDeck
+         * @params {List<Card>} deck
+         * @returns {void}
+         */
+        public static void ShuffleDeck(List<Card> deck)
+        {
+            //shape of Random this is not object
+            //create a pseudo random number sequence and stores it in random
+            Random random = new Random();
+            //record List number of cards in the deck List
+
+            int cardCount = deck.Count;
+           //iterate through the list of cards
+            for (int currentCard = 0;currentCard<cardCount;currentCard++)
+            {
+
+                Card tempCard = deck[currentCard];//copy current card to temp location
+                int randomCard = random.Next(0, cardCount);//get a random Card index
+                deck[currentCard] = deck[randomCard];//copy value from randomCard to currentCard
+                deck[randomCard] = tempCard; // copy current Card to random card
+            }// end - for loop
+
+
+        }
+
     }//close Program
 }//close nameSpace
      
